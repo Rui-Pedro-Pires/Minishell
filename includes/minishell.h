d37b6a93d708c7573b37f42ff477c2c99f17c0a7
ruiolive <ruipedro.pires@hotmail.com>
2024-01-18 14:15:05 +0000

    teste

---
+++ includes/minishell.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ruiolive  <ruiolive@student.42.fr   >      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/18 12:02:05 by ruiolive          #+#    #+#             */
+/*   Updated: 2024/01/18 12:02:05 by ruiolive         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "./Libft/libft.h"
