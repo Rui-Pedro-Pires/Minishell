993a6b256513e90a84e9717b7d1f74101d1f8386
Jorge Costa <jorteixe@student.42porto.com>
2024-01-30 14:31:25 +0000

    error handler no longer handles frees, all made void

--- src/error_handler.c
+++ src/error_handler.c
@@ -16,20 +16,20 @@ void	error_handler(int error_msg, void *param, void **param2)
 {
 	if (error_msg == ERROR_UNFINISHED_QUOTE)
 	{
+		(void)param;
 		(void)param2;
 		printf("Unfinished Quote\n");
-		free(param);
 	}
 	else if (error_msg == ERROR_SPECIAL_CHAR)
 	{
+		(void)param;
 		(void)param2;
 		printf("Wrong Special Char\n");
-		free(param);
 	}
 	else if (error_msg == ERROR_WRONG_INPUT)
 	{
+		(void)param;
 		(void)param2;
 		printf("Wrong input\n");
-		free(param);
 	}
 }
