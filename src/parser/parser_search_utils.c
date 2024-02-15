2a3644fd6a72a846f442f82d83bec6c0d2c2dbad
Kelho <Kelho88@pm.me>
2024-02-15 15:53:29 +0000

    corrected path for parser

--- src/parser/parser_search_utils.c
+++ src/parser/parser_search_utils.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
+#include "../../includes/minishell.h"
 
 char	*search_char(char *input)
 {
