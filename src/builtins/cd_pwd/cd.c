9b0ecafa6f000288498b602ecd89d0f3d126e655
Kelho <Kelho88@pm.me>
2024-03-12 15:06:39 +0000

    Update cd.c with new functions and fix bugs

--- src/builtins/cd_pwd/cd.c
+++ src/builtins/cd_pwd/cd.c
@@ -12,28 +12,50 @@
 
 #include "../../../includes/minishell.h"
 
-void	update_old_pwd(t_envs *head);
+void	update_old_pwd(t_pipes *node);
+void	update_current_pwd(t_pipes *node);
 
-int	ft_cd(t_envs *head, char **str)
+void	cd_log(t_pipes *node)
+{
+	t_envs	*current;
+	t_envs	*current_sorted;
+	char	*temp_dir;
+	char	*temp_dir_sorted;
+
+	current = node->init.envs;
+	current_sorted = node->init.sorted_envs;
+	temp_dir = ft_getenv(current, "OLDPWD");
+	temp_dir_sorted = ft_getenv(current_sorted, "OLDPWD");
+	printf("Normal: %s\n", temp_dir);
+	printf("Sorted: %s\n", temp_dir_sorted);
+	free(temp_dir);
+	free(temp_dir_sorted);
+}
+
+int	ft_cd(t_pipes *node, char **str)
 {
 	char	*new_dir;
+	t_envs	*current;
 
-	update_old_pwd(head);
+	current = node->init.envs;
+	update_old_pwd(node);
 	if (str[1] == NULL)
 	{
-		new_dir = ft_getenv(head, "HOME");
+		new_dir = ft_getenv(current, "HOME");
 		chdir(new_dir);
 		free(new_dir);
 		new_dir = NULL;
+		update_current_pwd(node);
 		return (1);
 	}
 	if (str[2] != NULL)
-		return (printf("\nruiolive&&jorteixe@minishell: cd: too many arguments\n"), 0);
+		return (printf("\nruiolive&&jorteixe@minishell: cd: too many arguments\n"),
+			0);
 	else
 	{
 		if (strcmp(str[1], "") == 0 || strcmp(str[1], "~") == 0
 			|| str[1] == NULL)
-			new_dir = ft_getenv(head, "HOME");
+			new_dir = ft_getenv(current, "HOME");
 		else
 			new_dir = ft_strdup(str[1]);
 		if (chdir(new_dir) == (-1))
@@ -41,8 +63,10 @@ int	ft_cd(t_envs *head, char **str)
 			err_num_chdir(new_dir);
 			free(new_dir);
 			new_dir = NULL;
+			update_current_pwd(node);
 			return (0);
 		}
+		update_current_pwd(node);
 		free(new_dir);
 	}
 	return (1);
@@ -52,14 +76,14 @@ void	err_num_chdir(char *str)
 {
 	if (errno == ENOENT)
 		printf("\nruiolive&&jorteixe@minishell:"
-			"cd: %s: No such file or directory\n",
-			str);
+				"cd: %s: No such file or directory\n",
+				str);
 	else if (errno == ENOTDIR)
 		printf("\nruiolive&&jorteixe@minishell: cd: %s: Not a directory\n",
 			str);
 }
 
-void	update_old_pwd(t_envs *head)
+void	update_old_pwd(t_pipes *node)
 {
 	char	*old_dir;
 	char	*name;
@@ -76,7 +100,28 @@ void	update_old_pwd(t_envs *head)
 	str_array[0] = NULL;
 	str_array[1] = total;
 	str_array[2] = NULL;
-	ft_export(head, str_array);
+	ft_export(node, str_array);
+	free(total);
+	free(old_dir);
+}
+void	update_current_pwd(t_pipes *node)
+{
+	char *old_dir;
+	char *name;
+	char *str_array[2];
+	char *total;
+	int total_size;
+
+	old_dir = getcwd(NULL, 0);
+	name = "PWD=";
+	total_size = ft_strlen(name) + ft_strlen(old_dir) + 1;
+	total = malloc(total_size);
+	ft_strlcpy(total, name, ft_strlen(name) + 1);
+	ft_strlcat(total, old_dir, total_size);
+	str_array[0] = NULL;
+	str_array[1] = total;
+	str_array[2] = NULL;
+	ft_export(node, str_array);
 	free(total);
 	free(old_dir);
 }
