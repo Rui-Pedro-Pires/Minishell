a5cc1d38332ebdcbecf5a4dc477ec58bc4555cff
Kelho <Kelho88@pm.me>
2024-03-13 15:18:43 +0000

    Refactor ft_echo function to remove unnecessary return statements

--- src/builtins/echo/echo.c
+++ src/builtins/echo/echo.c
@@ -17,7 +17,7 @@ bool	is_valid_no_new_line(char *str);
 void	print_d_array_with_newline_control(char **str_array, bool new_line);
 void	print_d_array_aux(char **str_array, int i);
 
-int	ft_echo(char **str_array) //#TODO Return EXIT_FAILURE or EXIT_SUCCESS
+int	ft_echo(char **str_array)
 {
 	bool new_line;
 	int i;
@@ -27,13 +27,13 @@ int	ft_echo(char **str_array) //#TODO Return EXIT_FAILURE or EXIT_SUCCESS
 	if (!str_array[i])
 	{
 		printf("\n");
-		return (EXIT_SUCCESS);
+		return (1);
 	}
 	check_newline(str_array[1], &new_line);
 	if (str_array[2] == NULL && new_line == false)
-		return (EXIT_SUCCESS);
+		return (1);
 	print_d_array_with_newline_control(str_array, new_line);
-	return (EXIT_SUCCESS);
+	return (1);
 }
 
 void	check_newline(char *str, bool *new_line)
