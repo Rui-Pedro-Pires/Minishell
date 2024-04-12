0d5df34e097a4dea0ee7c8d925fef69e9d72d0d8
Jorge Costa <Kelho88@pm.me>
2024-04-12 11:54:14 +0100

    refactor main to have less than 25 lines

--- src/main.c
+++ src/main.c
@@ -9,10 +9,11 @@
 /*   Updated: 2024/04/06 11:49:05 by ruiolive         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "../includes/minishell.h"
 
-int	g_return_value;
+int		g_return_value;
+void	initialize(t_init *init, char **env);
+void	process_input(t_pipes **head, char *input, t_init *init);
 
 int	main(int argc, char **argv, char **env)
 {
@@ -20,41 +21,52 @@ int	main(int argc, char **argv, char **env)
 	t_counter	count_struc;
 	t_pipes		*head;
 	t_init		init;
-	int			heardoc_index;
 
 	head = NULL;
-	init.envs = create_env_list(env);
-	init.sorted_envs = create_env_list(env);
-	init.sorted_envs = bubble_sort(init.sorted_envs);
-	(void) argv;
-	(void) argc;
+	initialize(&init, env);
+	(void)argv;
+	(void)argc;
 	while (1)
 	{
 		handle_iterative_mode();
 		count_struc.i = 0;
 		count_struc.counter = 0;
-		init.heardocs = NULL;
-		heardoc_index = -1;
-		init.heardoc_index = &heardoc_index;
 		input = line_read(&init, &count_struc);
-		if (input && *input)
-			add_history(input);
-		else
-			continue ;
-		if (check_for_dbpipe_dbamper(input))
-		{
-			creat_list(&head, input, &init, 1);
-			g_return_value = recursive_executer(head, 0);
-		}
-		else
-		{
-			creat_list(&head, input, &init, 0);
-			g_return_value = list_iterator_executer(head);
-		}
+		process_input(&head, input, &init);
 		free_input(&input);
 		free_heardoc(head);
 		free_list(&head);
 	}
 	free_env_list(init.envs);
 	rl_clear_history();
+}
+
+void	initialize(t_init *init, char **env)
+{
+	init->envs = create_env_list(env);
+	init->sorted_envs = create_env_list(env);
+	init->sorted_envs = bubble_sort(init->sorted_envs);
+}
+
+void	process_input(t_pipes **head, char *input, t_init *init)
+{
+	int	heardoc_index;
+
+	heardoc_index = -1;
+	init->heardocs = NULL;
+	init->heardoc_index = &heardoc_index;
+	if (input && *input)
+		add_history(input);
+	else
+		return ;
+	if (check_for_dbpipe_dbamper(input))
+	{
+		creat_list(head, input, init, 1);
+		g_return_value = recursive_executer(*head, 0);
+	}
+	else
+	{
+		creat_list(head, input, init, 0);
+		g_return_value = list_iterator_executer(*head);
+	}
 }
