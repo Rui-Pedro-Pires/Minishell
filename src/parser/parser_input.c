3c2390aecb1ace9640676976dae04da2e5d756f1
Rui-Pedro-Pires <ruiolive@student.42porto.com>
2024-04-17 14:07:22 +0100

    Fix whitespace handling in check_begin_case_parser function

--- src/parser/parser_input.c
+++ src/parser/parser_input.c
@@ -29,7 +29,7 @@ int	check_begin_case_parser(char *input, int *i)
 {
 	int	checker;
 
-	while (ft_strchr(" \t\n", input[(*i)]))
+	while (input[(*i)] && ft_strchr(" \t\n", input[(*i)]))
 		(*i)++;
 	if (!input[(*i)])
 		return (0);
